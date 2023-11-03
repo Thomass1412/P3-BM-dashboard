e1875bf84083d57f01730d419c89f67570c79274
ollioddi <oliver@roedscholer.dk>
2023-11-03 10:41:23 +0100

    smoll release

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # P3-BM-dashboard
+#### Run docker in production
+docker compose --profile prod up --build
+
+- "prod" is the profile which runs the backend in docker.
+  - spring-boot run does not run the backend in docker. It skips building.
+- --build forces a new build.
---
+++ backend/src/main/java/com/aau/p3/performancedashboard/PerformancedashboardApplication.java
@@ -0,0 +1,13 @@
+package com.aau.p3.performancedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformancedashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformancedashboardApplication.class, args);
+	}
+
+}
