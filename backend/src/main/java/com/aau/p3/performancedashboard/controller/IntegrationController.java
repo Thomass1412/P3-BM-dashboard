6053a7ca6e7cf77c00c0884e0e66fed8eb3b9526
ollioddi <oliver@roedscholer.dk>
2023-11-03 13:49:07 +0100

    Initial API Co-authored-by: Jessica Tegner <jessica@jessicategner.com> Co-authored-by: Thomass1412 <Thomass1412@users.noreply.github.com>

--- backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
@@ -1,12 +1,9 @@
-package com.aau.p3.performancedashboard.controller;
+// $postParams = @{id='id123';title='integration title here'} Invoke-WebRequest -Uri http://localhost:8080/api/v1/integration -Method POST -Body $postParams
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+package com.aau.p3.performancedashboard.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +13,42 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aau.p3.performancedashboard.model.Integration;
+import com.aau.p3.performancedashboard.service.IntegrationService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
-@RequestMapping("/api/v1/integrations")
+@RequestMapping("/api/v1/integration")
 public class IntegrationController {
+    
+    @Autowired
+    IntegrationService integrationService;
+
     @GetMapping("/")
-    public String getIntegrations() {
-        return "bob";
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<Integration> getAllIntegrations(@RequestParam(required = false) String title) {
+        if(title == null) {
+            return integrationService.findAll();
+        } else {
+            return integrationService.findByTitleContaining(title);
+        }
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Integration> getIntegrationById(@PathVariable("id") String id) {
+        return integrationService.findById(id);
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Integration> createIntegration(@RequestBody Integration integration) {
+        return integrationService.save(new Integration(integration.getId(), integration.getTitle()));
     }
 }
