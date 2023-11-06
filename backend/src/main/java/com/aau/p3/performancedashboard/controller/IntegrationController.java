d9b37ff92f3848cd1a9a425f86d01cd9f421d14d
ollioddi <oliver@roedscholer.dk>
2023-11-06 15:48:23 +0100

    Cors

--- backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
@@ -23,6 +23,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1")
 public class IntegrationController {
     
