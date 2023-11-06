056a85f2e367e7836e5ef69b4bb54dede3c9d445
MBryder <mbryde22@student.aau.dk>
2023-11-06 15:12:19 +0100

    Update IntegrationForm.tsx

--- frontend/src/app/Components/IntegrationForm.tsx
+++ frontend/src/app/Components/IntegrationForm.tsx
@@ -44,6 +44,7 @@ function IntegrationForm() {
 
   return (
     <form onSubmit={handleSubmit}>
+<<<<<<< HEAD
       <div>
         <label htmlFor="id">ID:</label>
         <input
@@ -65,6 +66,29 @@ function IntegrationForm() {
         />
       </div>
       <button type="submit">Submit</button>
+=======
+        <label className="mx-2" htmlFor="id">ID:
+          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+            type="text"
+            id="id"
+            name="id"
+            value={formData.id}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="title">Title:
+          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+        </label>
+      <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5" 
+        type="submit">Submit
+      </button>
+>>>>>>> parent of be5ab45 (Merge branch 'frontend-getandpost' of https://github.com/Thomass1412/P3-BM-dashboard into frontend-getandpost)
     </form>
   );
 }
