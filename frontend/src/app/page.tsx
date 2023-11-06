096627a47868504d7b04da8b7c241f96828d0587
MBryder <mbryde22@student.aau.dk>
2023-11-06 17:30:32 +0100

    Revert "Merge branch 'frontend-getandpost' of https://github.com/Thomass1412/P3-BM-dashboard into frontend-getandpost"
    This reverts commit 9bd9b3fcb9102d1a601fef81cf796296027f46ac, reversing
    changes made to 03a3e4509d4a3f242be68dc5ff5cdda7a7192e42.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
             Get integration
           </h1>
           <div className="rounded-3xl w-full h-20 pb-5 mb-2 bg-slate-200"></div>
+          <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5">
+            Get button
+          </button>
         </div>
         <div className="w-full m-5 rounded-3xl bg-sky-950 flex flex-col items-center justify-between p-2">
           <h1 className="lg:text-4xl mb-5 mx-10 mt-5 text-4xl font-extrabold">
