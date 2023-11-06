e4a078c165c1b2c7d3ec7f82994ce950c92476d0
eli8811 <newworldlibraryelias867@gmail.com>
2023-11-06 13:38:31 +0100

    initial post component integration

---
+++ frontend/src/app/Components/IntegrationForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useState } from "react";
+
+function IntegrationForm() {
+  // State to store the form data
+  const [formData, setFormData] = useState({
+    id: "",
+    title: "",
+  });
+
+  // Function to handle the form input changes
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Function to handle the form submission
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost/api/v1/integration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: formData.id,
+          title: formData.title,
+        }),
+      });
+      const data = await response.json();
+      // Handle success response
+      console.log("Success:", data);
+    } catch (error) {
+      // Handle errors here
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input
+          type="text"
+          id="id"
+          name="id"
+          value={formData.id}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default IntegrationForm;
