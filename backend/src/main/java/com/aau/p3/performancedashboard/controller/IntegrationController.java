9813d405fe1c5a8814f8d08451b9d2032c731719
ollioddi <oliver@roedscholer.dk>
2023-11-06 16:08:25 +0100

    Test

--- backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
@@ -23,7 +23,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1")
 public class IntegrationController {
     
