5cd3eba7d0088e025fee516d4f3d1c56aa50e0c9
MBryder <mbryde22@student.aau.dk>
2023-11-06 14:10:02 +0100

    mini mer

--- frontend/src/app/Components/IntegrationForm.tsx
+++ frontend/src/app/Components/IntegrationForm.tsx
@@ -44,27 +44,27 @@ function IntegrationForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="id">ID:</label>
-        <input
-          type="text"
-          id="id"
-          name="id"
-          value={formData.id}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={formData.title}
-          onChange={handleChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
+        <label className="mx-2" htmlFor="id">ID:
+          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+            type="text"
+            id="id"
+            name="id"
+            value={formData.id}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="title">Title:
+          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+        </label>
+      <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5" 
+        type="submit">Submit
+      </button>
     </form>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,24 @@ import IntegrationForm from "./Components/IntegrationForm";
 
 export default function Home() {
   return (
-    <>
-      <p>Hello</p>
-      <IntegrationForm />
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-16 bg-sky-900">
+      <div className="flex w-4/5 flex-row items-center justify-between p-2">
+        <div className="w-full m-5 rounded-3xl bg-sky-950 flex flex-col items-center justify-between p-2">
+          <h1 className="lg:text-4xl mb-5 mx-10 mt-5 text-4xl font-extrabold">
+            Get integration
+          </h1>
+          <div className="rounded-3xl w-full h-20 pb-5 mb-2 bg-slate-200"></div>
+          <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5">
+            Get button
+          </button>
+        </div>
+        <div className="w-full m-5 rounded-3xl bg-sky-950 flex flex-col items-center justify-between p-2">
+          <h1 className="lg:text-4xl mb-5 mx-10 mt-5 text-4xl font-extrabold">
+            Post integration
+          </h1>
+          <IntegrationForm />
+        </div>
+      </div>
+    </main>
   );
 }
