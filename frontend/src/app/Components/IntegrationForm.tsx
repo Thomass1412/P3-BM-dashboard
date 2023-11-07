d88e38e8faaa2ecf02c295e98f4238b5cf0f7f97
ollioddi <oliver@roedscholer.dk>
2023-11-07 10:26:56 +0100

    Smoll fix

--- frontend/src/app/Components/IntegrationForm.tsx
+++ frontend/src/app/Components/IntegrationForm.tsx
@@ -21,10 +21,15 @@ function IntegrationForm() {
   // Function to handle the form submission
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+
+    /*
+      Access to fetch at 'http://localhost/api/v1/integration' from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+    */
     try {
       const response = await fetch("http://localhost/api/v1/integration", {
         method: "POST",
-        mode: "cors",
+        mode: "cors", // See comment above
+        credentials: "omit",
         headers: {
           "Content-Type": "application/json",
         },
