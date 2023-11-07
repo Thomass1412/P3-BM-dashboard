a614f9d6dd65bb4204cd00ee85afa26ed234c030
MBryder <mbryde22@student.aau.dk>
2023-11-07 11:58:31 +0100

    get done

--- frontend/src/app/Components/GetIntegration.tsx
+++ frontend/src/app/Components/GetIntegration.tsx
@@ -4,28 +4,31 @@ import React, { useEffect, useState } from "react";
 
 function GetIntegration() {
 
-    const [data, setData] = useState({
+    const [data, setData] = useState([{
         id: "",
         title: "",
-    });
+      }]);
 
     const getData = async ()=>{
         try{
             const response = await fetch("http://localhost/api/v1/integration")
-            
-            const ddata = await response.json();
-            console.log("Success:", ddata);
-            setData(ddata);
-
+            const data = await response.json();
+            setData(data);
+            console.log("Success:", data);
         }catch(error){
             console.error("Error:", error);
-        }
-        
+        } 
     }
+    const listitems = data.map((integration) => (
+        <li className="py-0" key={integration.id}>{integration.id} {integration.title}</li>
+    ))
+
+
+    
 
     return (
-        <><div className="text-black rounded-3xl w-full h-20 pb-5 mb-2 bg-slate-200 py-2 px-4">
-           hej
+        <><div className="text-black rounded w-full h-20 pb-2 mb-4 bg-slate-200">
+            <ul className="m-1 pl-2 max-h-full border-white overflow-y-scroll">{listitems}</ul>
         </div>
         <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5" onClick={getData}>
             Get button
