357151b23a89e962c984a9e2efdca03af3c34a5a
MBryder <mbryde22@student.aau.dk>
2023-11-07 10:18:21 +0100

    fix get

--- frontend/src/app/Components/GetIntegration.tsx
+++ frontend/src/app/Components/GetIntegration.tsx
@@ -9,12 +9,14 @@ function GetIntegration() {
         title: "",
     });
 
-
     const getData = async ()=>{
         try{
-            const response = fetch("http://localhost/api/v1/integration")
-            const ddata =  (await response).json;
-            console.log("Success:", data);
+            const response = await fetch("http://localhost/api/v1/integration")
+            
+            const ddata = await response.json();
+            console.log("Success:", ddata);
+            setData(ddata);
+
         }catch(error){
             console.error("Error:", error);
         }
@@ -23,7 +25,7 @@ function GetIntegration() {
 
     return (
         <><div className="text-black rounded-3xl w-full h-20 pb-5 mb-2 bg-slate-200 py-2 px-4">
-            hej
+           hej
         </div>
         <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5" onClick={getData}>
             Get button
