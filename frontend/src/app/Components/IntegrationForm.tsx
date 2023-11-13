50a45cd49bfd14978b5b7d3509a32369add9cef3
eli8811 <newworldlibraryelias867@gmail.com>
2023-11-13 12:07:27 +0100

    changed api db endpoint

--- frontend/src/app/Components/IntegrationForm.tsx
+++ frontend/src/app/Components/IntegrationForm.tsx
@@ -26,7 +26,7 @@ function IntegrationForm() {
       Access to fetch at 'http://localhost/api/v1/integration' from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
     */
     try {
-      const response = await fetch("http://localhost/api/v1/integration", {
+      const response = await fetch("http://localhost/api/v1/integrations", {
         method: "POST",
         mode: "cors", // See comment above
         credentials: "omit",
@@ -49,26 +49,33 @@ function IntegrationForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-        <label className="mx-2" htmlFor="id">ID:
-          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
-            type="text"
-            id="id"
-            name="id"
-            value={formData.id}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="title">Title:
-          <input className="w-full rounded-3xl mb-3 text-black py-1 px-2"
-            type="text"
-            id="title"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </label>
-      <button className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5" 
-        type="submit">Submit
+      <label className="mx-2" htmlFor="id">
+        ID:
+        <input
+          className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+          type="text"
+          id="id"
+          name="id"
+          value={formData.id}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="title">
+        Title:
+        <input
+          className="w-full rounded-3xl mb-3 text-black py-1 px-2"
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </label>
+      <button
+        className="bg-sky-50 bg-opacity-50 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full mb-5"
+        type="submit"
+      >
+        Submit
       </button>
     </form>
   );
