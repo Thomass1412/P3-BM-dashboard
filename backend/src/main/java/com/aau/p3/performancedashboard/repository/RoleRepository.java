c2635cf772a5e0fe90f6173b7425a7189dd96fe0
JessicaTegner <jessica.tegner@outlook.com>
2023-11-20 10:03:27 +0100

    Updated MongoRepo to ReactiveMongoRepo

--- backend/src/main/java/com/aau/p3/performancedashboard/repository/RoleRepository.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/repository/RoleRepository.java
@@ -2,11 +2,12 @@ package com.aau.p3.performancedashboard.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
 
 import com.aau.p3.performancedashboard.model.ERole;
 import com.aau.p3.performancedashboard.model.Role;
 
-public interface RoleRepository extends MongoRepository<Role, String> {
+public interface RoleRepository extends ReactiveMongoRepository<Role, String> {
   Optional<Role> findByName(ERole name);
 }
--- backend/src/main/java/com/aau/p3/performancedashboard/repository/UserRepository.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/repository/UserRepository.java
@@ -2,13 +2,14 @@ package com.aau.p3.performancedashboard.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.aau.p3.performancedashboard.model.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
   Optional<User> findByUsername(String username);
 
   Boolean existsByUsername(String username);
