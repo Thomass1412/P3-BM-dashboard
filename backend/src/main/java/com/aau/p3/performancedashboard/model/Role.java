556bc995b0f85266f43ae9083e41f0c984ea61d0
JessicaTegner <jessica.tegner@outlook.com>
2023-11-20 13:35:02 +0100

    Updated to use anotations where useful

--- backend/src/main/java/com/aau/p3/performancedashboard/model/Role.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/model/Role.java
@@ -3,34 +3,21 @@ package com.aau.p3.performancedashboard.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
 @Document(collection = "roles")
 public class Role {
+  @Getter
+  @Setter
   @Id
   private String id;
 
+  @Getter
+  @Setter
   private ERole name;
 
-  public Role() {
 
-  }
-
-  public Role(ERole name) {
-    this.name = name;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public ERole getName() {
-    return name;
-  }
-
-  public void setName(ERole name) {
-    this.name = name;
-  }
 }
--- backend/src/main/java/com/aau/p3/performancedashboard/model/User.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/model/User.java
@@ -7,26 +7,40 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import lombok.Setter;
+import lombok.Getter;
 
 @Document(collection = "users")
 public class User {
+  @Id
+  @Getter
+  @Setter
   private String id;
 
   @NotBlank
   @Size(max = 20)
+  @Getter
+  @Setter
   private String username;
 
   @NotBlank
+  @Getter
+  @Setter
   @Size(max = 50)
   @Email
   private String email;
 
+  @Getter
+  @Setter
   @NotBlank
   @Size(max = 120)
   private String password;
 
+  @Getter
+  @Setter
   @DocumentReference
   private Set<Role> roles = new HashSet<>();
 
@@ -40,43 +54,4 @@ public class User {
     this.password = password;
   }
 
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
-  }
 }
