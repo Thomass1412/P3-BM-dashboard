ffceea3d63c702bb953feb6a32079bf4cea9a30c
ollioddi <oliver@roedscholer.dk>
2023-11-23 17:05:38 +0100

    Refactor with annotations

--- backend/src/main/java/com/aau/p3/performancedashboard/payload/response/MessageResponse.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/payload/response/MessageResponse.java
@@ -1,17 +1,12 @@
 package com.aau.p3.performancedashboard.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
 public class MessageResponse {
   private String message;
-
-  public MessageResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
 }
