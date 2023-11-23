dd98fe9a4b614486cd5eea670d4389d0e389c6d2
ollioddi <oliver@roedscholer.dk>
2023-11-23 17:03:57 +0100

    Refactor with annotations

--- backend/src/main/java/com/aau/p3/performancedashboard/payload/request/LoginRequest.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/payload/request/LoginRequest.java
@@ -1,27 +1,16 @@
-  package com.aau.p3.performancedashboard.payload.request;
+package com.aau.p3.performancedashboard.payload.request;
 
-  import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
-  public class LoginRequest {
-    @NotBlank
-    private String username;
+@Getter
+@Setter
+public class LoginRequest {
+  @NotBlank
+  private String username;
 
-    @NotBlank
-    private String password;
+  @NotBlank
+  private String password;
 
-    public String getUsername() {
-      return username;
-    }
-
-    public void setUsername(String username) {
-      this.username = username;
-    }
-
-    public String getPassword() {
-      return password;
-    }
-
-    public void setPassword(String password) {
-      this.password = password;
-    }
-  }
+}
