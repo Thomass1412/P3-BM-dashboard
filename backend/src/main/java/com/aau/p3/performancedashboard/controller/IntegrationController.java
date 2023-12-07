f620efe7e5be4d9ddbb8bb4c6b37479211cecaae
ollioddi <oliver@roedscholer.dk>
2023-12-07 23:10:31 +0100

    Cleanup, Document and error handling

--- backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/controller/IntegrationController.java
@@ -1,28 +1,11 @@
 package com.aau.p3.performancedashboard.controller;
 
-import com.aau.p3.performancedashboard.dto.CustomResponse;
-import com.aau.p3.performancedashboard.dto.IntegrationDTO;
-import com.aau.p3.performancedashboard.exceptions.IntegrationNotFoundException;
-import com.aau.p3.performancedashboard.service.IntegrationService;
-
-// SWAGGER dependencies
-// DOCS https://docs.swagger.io/swagger-core/v2.0.0-RC3/apidocs/io/swagger/v3/oas/annotations
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-
-import com.aau.p3.performancedashboard.model.Integration;
-import com.aau.p3.performancedashboard.model.IntegrationData;
-import com.aau.p3.performancedashboard.payload.response.PageableResponse;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.data.domain.Page;
@@ -32,8 +15,44 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.aau.p3.performancedashboard.exceptions.IntegrationNotFoundException;
+import com.aau.p3.performancedashboard.model.Integration;
+import com.aau.p3.performancedashboard.payload.request.CreateIntegrationDataRequest;
+import com.aau.p3.performancedashboard.payload.request.CreateIntegrationRequest;
+import com.aau.p3.performancedashboard.payload.response.ErrorResponse;
+import com.aau.p3.performancedashboard.payload.response.IntegrationDataResponse;
+import com.aau.p3.performancedashboard.payload.response.IntegrationResponse;
+import com.aau.p3.performancedashboard.payload.response.PageableResponse;
+import com.aau.p3.performancedashboard.service.IntegrationDataService;
+import com.aau.p3.performancedashboard.service.IntegrationService;
+
+import com.mongodb.MongoWriteException;
+import com.mongodb.WriteError;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// SWAGGER dependencies
+// DOCS https://docs.swagger.io/swagger-core/v2.0.0-RC3/apidocs/io/swagger/v3/oas/annotations
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
 
 /**
@@ -55,9 +74,12 @@ class IntegrationsController {
     private IntegrationService integrationService;
 
     /**
-     * This constructor is used to inject the {@link IntegrationService} dependency.
+     * This constructor is used to inject the {@link IntegrationService} and
+     * {@link IntegrationDataService} dependencies.
      *
-     * @param integrationService The service to interact with the data layer.
+     * @param integrationService     The service to interact with the data layer.
+     * @param integrationDataService The service to interact with the integration
+     *                               data.
      */
     @Autowired
     public IntegrationsController(IntegrationService integrationService) {
@@ -113,39 +135,45 @@ class IntegrationsController {
 @RequestMapping("/api/v1/integration")
 public class IntegrationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(IntegrationController.class);
+
     // Service to interact with the data layer.
     private IntegrationService integrationService;
+    private IntegrationDataService integrationDataService;
 
     /**
-     * This constructor is used to inject the {@link IntegrationService} dependency.
+     * This constructor is used to inject the {@link IntegrationService} and
+     * {@link IntegrationDataService} dependencies.
      *
-     * @param integrationService The service to interact with the data layer.
+     * @param integrationService     The service to interact with the data layer.
+     * @param integrationDataService The service to interact with the integration
+     *                               data.
      */
     @Autowired
-    public IntegrationController(IntegrationService integrationService) {
+    public IntegrationController(IntegrationService integrationService, IntegrationDataService integrationDataService) {
         this.integrationService = integrationService;
+        this.integrationDataService = integrationDataService;
     }
 
-    @Operation(summary = "Instantiate a new integration", description = "The request body must include an unique name and a predefined type. \\['internal'\\]")
+    @Operation(summary = "Instantiate a new integration", description = "The request body must include a unique name and a predefined type. ['internal']")
     @ApiResponses({
             @ApiResponse(responseCode = "201", content = {
-                    @Content(array = @ArraySchema(schema = @Schema(implementation = Integration.class)), mediaType = "application/json") }, description = "Successfully created new integration"),
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = IntegrationResponse.class)), mediaType = "application/json") }, description = "Successfully created a new integration"),
             @ApiResponse(responseCode = "400", content = {
-                    @Content(array = @ArraySchema(schema = @Schema(implementation = CustomResponse.class)), mediaType = "application/json") }, description = "Bad request. Invalid parameters or requested integration already exists."),
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = ErrorResponse.class)), mediaType = "application/json") }, description = "Bad request. Invalid parameters or requested integration already exists."),
             @ApiResponse(responseCode = "404", description = "Integration type not found."),
             @ApiResponse(responseCode = "500", description = "Generic Internal Server Error."),
     })
     @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Mono<IntegrationResponse>> createIntegration(@RequestBody @Valid IntegrationDTO integrationDTO)
-            throws Exception {
-        return ResponseEntity.ok()
-                .body(integrationService.saveIntegration(integrationDTO));
+    public ResponseEntity<Mono<IntegrationResponse>> createIntegration(
+            @RequestBody @Valid CreateIntegrationRequest createIntegrationRequest) {
+        return ResponseEntity.ok().body(integrationService.createIntegration(createIntegrationRequest));
     }
 
-    @Operation(summary = "Instantiate new integrationData", description = "The request body must include valid integrationData and path variable must contain an existing integrationId.")
+    @Operation(summary = "Submit new integrationData", description = "The request body must include valid integrationData and path variable must contain an existing integrationId.")
     @ApiResponses({
             @ApiResponse(responseCode = "201", content = {
-                    @Content(array = @ArraySchema(schema = @Schema(implementation = IntegrationData.class)), mediaType = "application/json") }, description = "Successfully created new integrationData"),
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = IntegrationDataResponse.class)), mediaType = "application/json") }, description = "Successfully created new integrationData"),
             @ApiResponse(responseCode = "400", description = "Bad request. Missing integrationData"),
             @ApiResponse(responseCode = "404", description = "Integration with id not found"),
             @ApiResponse(responseCode = "500"),
@@ -153,19 +181,18 @@ public class IntegrationController {
 
     @PostMapping("/{integrationId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<IntegrationData> createIntegrationData(@PathVariable String integrationId,
-            @RequestBody IntegrationData integrationData) throws Exception {
-        Integration integration = integrationService.findById(integrationId).block();
-        Mono<IntegrationData> res = integration.saveIntegrationData(integrationData);
-        return res;
+    public ResponseEntity<Mono<IntegrationDataResponse>> submitIntegrationData(@PathVariable String integrationId,
+            @RequestBody @Valid CreateIntegrationDataRequest integrationDataRequest) throws Exception {
+        return ResponseEntity.ok()
+                .body(integrationDataService.saveIntegrationData(integrationId, integrationDataRequest));
     }
 
     @Operation(summary = "Retrieve an integration by its ID", description = "Fetches a single integration based on its unique ID.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Integration.class)), description = "Successfully retrieved the integration"),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = CustomResponse.class)), description = "Integration not found")
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class)), description = "Integration not found")
     })
-    @GetMapping("/{integrationId}")
+    @GetMapping("/{integrationId}") // This must be converted to use IntegrationResponse
     public Mono<ResponseEntity<Integration>> getIntegrationById(@PathVariable String integrationId) {
         return integrationService.findById(integrationId)
                 .map(integration -> ResponseEntity.ok().body(integration));
@@ -173,28 +200,28 @@ public class IntegrationController {
 
     @ResponseBody
     @ExceptionHandler(IntegrationNotFoundException.class)
-    public ResponseEntity<CustomResponse> handleNotFoundException(IntegrationNotFoundException ex) {
-        CustomResponse response = new CustomResponse("Integration not found", "error", List.of(ex.getMessage()));
+    public ResponseEntity<ErrorResponse> handleNotFoundException(IntegrationNotFoundException ex) {
+        ErrorResponse response = new ErrorResponse("Integration not found", "error", List.of(ex.getMessage()));
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CustomResponse> handleException(Exception ex) {
-        CustomResponse response = new CustomResponse(ex.getMessage(), "error", "Internal Server Error");
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), "error", "Internal Server Error");
         return ResponseEntity.internalServerError().body(response);
     }
 
     @ResponseBody
     @ExceptionHandler(IncorrectResultSizeDataAccessException.class)
-    public ResponseEntity<CustomResponse> handleIncorrectResultSizeDataAccessException(
+    public ResponseEntity<ErrorResponse> handleIncorrectResultSizeDataAccessException(
             IncorrectResultSizeDataAccessException ex) {
-        CustomResponse response = new CustomResponse(ex.getMessage(), "error", "Bad Request");
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), "error", "Bad Request");
         return ResponseEntity.badRequest().body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CustomResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult()
                 .getAllErrors()
                 .stream()
@@ -207,7 +234,64 @@ public class IntegrationController {
                 })
                 .collect(Collectors.toList());
 
-        CustomResponse response = new CustomResponse("Validation Error", "error", errors);
+        ErrorResponse response = new ErrorResponse("Validation Error", "error", errors);
         return ResponseEntity.badRequest().body(response);
+    }
+
+    /**
+     * Handles the exception thrown when there is a write error in MongoDB.
+     * Extracts the details of the error and constructs a custom response with the
+     * error message and code.
+     * Returns a ResponseEntity with a bad request status and the custom response as
+     * the body.
+     *
+     * @param ex The MongoWriteException that was thrown
+     * @return A ResponseEntity containing a Mono of ErrorResponse
+     */
+    @ExceptionHandler(MongoWriteException.class)
+    public ResponseEntity<ErrorResponse> handleMongoWriteException(MongoWriteException ex) {
+        WriteError writeError = ex.getError();
+        String message = writeError.getMessage();
+        int code = writeError.getCode();
+        BsonDocument details = writeError.getDetails();
+
+        List<String> errors = new ArrayList<>();
+        if (details != null) {
+            errors.addAll(extractErrors(details));
+        } else {
+            errors.add(message);
+        }
+
+        String customMessage = String.format("Error writing to MongoDB. Code: %d.", code);
+        ErrorResponse response = new ErrorResponse(customMessage, message, errors);
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    private List<String> extractErrors(BsonValue value) {
+        List<String> errors = new ArrayList<>();
+        if (value.isDocument()) {
+            BsonDocument document = value.asDocument();
+            if (document.containsKey("required")) {
+                for (BsonValue field : document.getArray("required")) {
+                    errors.add(String.format("Missing required field: %s", field.asString().getValue()));
+                }
+            }
+            if (document.containsKey("properties")) {
+                for (BsonValue property : document.getDocument("properties").values()) {
+                    errors.addAll(extractErrors(property));
+                }
+            }
+            if (document.containsKey("type") && document.getString("type").getValue().equals("number")) {
+                String consideredValue = document.getString("consideredValue").getValue();
+                String consideredType = document.getString("consideredType").getValue();
+                errors.add(String.format("Type mismatch: Provided value: %s. Provided type: %s. Expected type: number.",
+                        consideredValue, consideredType));
+            }
+        } else if (value.isArray()) {
+            for (BsonValue element : value.asArray()) {
+                errors.addAll(extractErrors(element));
+            }
+        }
+        return errors;
     }
 }
