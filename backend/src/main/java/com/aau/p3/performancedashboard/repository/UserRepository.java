751919a2cec92617880681515ed7a60c88896543
ollioddi <oliver@roedscholer.dk>
2023-12-09 23:25:40 +0100

    Make repo reactive

--- backend/src/main/java/com/aau/p3/performancedashboard/repository/UserRepository.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/repository/UserRepository.java
@@ -1,18 +1,41 @@
 package com.aau.p3.performancedashboard.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 import org.springframework.stereotype.Repository;
 
 import com.aau.p3.performancedashboard.model.User;
 
+import reactor.core.publisher.Mono;
+
+/**
+ * Repository interface for {@link User} instances. Provides basic CRUD operations due to the extension of
+ * {@link ReactiveMongoRepository}. Includes custom implemented functionality by method signatures.
+ */
 @Repository
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
-  Optional<User> findByUsername(String username);
+  
+  /**
+   * Finds a {@link User} by their username.
+   *
+   * @param username the username of the User
+   * @return a Mono emitting the User if found, or empty if not
+   */
+  Mono<User> findByUsername(String username);
 
-  Boolean existsByUsername(String username);
+  /**
+   * Checks if a {@link User} exists by their username.
+   *
+   * @param username the username of the User
+   * @return a Mono emitting true if the User exists, false otherwise
+   */
+  Mono<Boolean> existsByUsername(String username);
 
-  Boolean existsByEmail(String email);
+  /**
+   * Checks if a {@link User} exists by their email.
+   *
+   * @param email the email of the User
+   * @return a Mono emitting true if the User exists, false otherwise
+   */
+  Mono<Boolean> existsByEmail(String email);
 }
