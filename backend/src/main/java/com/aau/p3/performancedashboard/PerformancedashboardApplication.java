ea4d4bc80737eb9a86ec0ea9580b47515702e60c
ollioddi <oliver@roedscholer.dk>
2023-12-11 16:09:51 +0100

    WIP

--- backend/src/main/java/com/aau/p3/performancedashboard/PerformancedashboardApplication.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/PerformancedashboardApplication.java
@@ -1,27 +1,47 @@
 package com.aau.p3.performancedashboard;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
+@Component
 @OpenAPIDefinition(info = @Info(title = "Performance Dashboard", version = "1.0", description = "Backend API v1.0"))
-// This is to tell spring where our repositories are located
 @ComponentScan("com.aau.p3.performancedashboard.repository")
 @ComponentScan("com.aau.p3.performancedashboard.security")
+@ComponentScan("com.aau.p3.performancedashboard.setup")
 public class PerformancedashboardApplication {
+
+	private final Setup setup;
+	private final Logger logger = LoggerFactory.getLogger(PerformancedashboardApplication.class);
+
+	@Autowired
+	public PerformancedashboardApplication(Setup setup) {
+		this.setup = setup;
+	}
+
+	@PostConstruct
+    public void init() {
+		logger.info("Starting setup steps");
+		try {
+			setup.run();
+		} catch (Exception e) {
+			logger.error("Error during setup steps", e);
+		}
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerformancedashboardApplication.class, args);
-
-		// Ensures that a default admin is set up on startup as of now.
-		Setup setup = new Setup();
-		setup.run();
 	}
 
 }
--- backend/src/main/java/com/aau/p3/performancedashboard/Setup.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/Setup.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import com.aau.p3.performancedashboard.model.ERole;
 import com.aau.p3.performancedashboard.model.Role;
@@ -18,54 +19,79 @@ import com.aau.p3.performancedashboard.model.User;
 import com.aau.p3.performancedashboard.repository.RoleRepository;
 import com.aau.p3.performancedashboard.repository.UserRepository;
 
+import lombok.Setter;
 import reactor.core.publisher.Mono;
 
+@Component
+@Setter
 public class Setup {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Logger logger = LoggerFactory.getLogger(Setup.class);
 
-	@Autowired
-	Environment environment;
+	private Environment environment;
+    private PasswordEncoder encoder;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    public Setup() {
+        // Default constructor
+    }
+
+    @Autowired
+    public Setup(Environment environment, PasswordEncoder encoder, UserRepository userRepository, RoleRepository roleRepository) {
+        this.environment = environment;
+        this.encoder = encoder;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
 
 	public void run() {
-		// put functions in here, that spring should execute on startup.
-		// this could be something that should be run on each startup, or
-		// something that should be run only once, during the first startup.
-		// functions themselfs are responsible for checking if they should be run or
-		// not.
+		logger.info("Processing setup steps");
 
-		this.logger.info("Processing setup steps");
+		createRoles();
 
-		// this.createAdminUser();
+		//createAdminUser();
 
-		this.logger.info("Finished processing setup steps");
+		logger.info("Finished processing setup steps");
 	}
 
-	@Autowired
-	PasswordEncoder encoder;
+	/*
+	   ROLE_TV,
+  ROLE_AGENT,
+  ROLE_SUPERVISOR,
+  ROLE_ADMIN
+	 */
 
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	RoleRepository roleRepository;
+	private void createRoles() {
+		// Only create, if not found
+		if (!roleRepository.findByName(ERole.ROLE_AGENT).hasElement().block()) {
+			Role agentRole = new Role(ERole.ROLE_AGENT);
+			roleRepository.save(agentRole).block();
+		} else if(!roleRepository.findByName(ERole.ROLE_SUPERVISOR).hasElement().block()) {
+			Role supervisorRole = new Role(ERole.ROLE_SUPERVISOR);
+			roleRepository.save(supervisorRole).block();
+		} else if(!roleRepository.findByName(ERole.ROLE_ADMIN).hasElement().block()) {
+			Role userRole = new Role(ERole.ROLE_ADMIN);
+			roleRepository.save(userRole).block();
+		} else if(!roleRepository.findByName(ERole.ROLE_TV).hasElement().block()) {
+			Role userRole = new Role(ERole.ROLE_TV);
+			roleRepository.save(userRole).block();
+		} else {
+			this.logger.info("Roles already created");
+			return;
+		}
+	}
 
 	private void createAdminUser() {
-		// check if admin user exists
-		// if not, create it
-		// if yes, do nothing
-
-		// check if admin user exists
+		// Dont create admin user if it already exists
 		if (userRepository.existsByUsername("admin").block()) {
-			// if yes, do nothing
 			return;
 		}
 
-		// if not, create it
-		// Create new user's account
+		// Create admin user
 		String id = UUID.randomUUID().toString();
-		User user = new User(id, environment.getProperty("admin.username"), environment.getProperty("admin.email"),
-				encoder.encode(environment.getProperty("admin.password")));
+		User user = new User(id, "admin", "admin@localhost",
+				encoder.encode("test"));
 		List<Role> roles = new LinkedList<>();
 		Role adminRole = roleRepository.findByName(ERole.ROLE_AGENT).switchIfEmpty(Mono.error(new RuntimeException("Error: Role is not found."))).block();
 		roles.add(adminRole);
