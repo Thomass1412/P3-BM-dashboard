d4cee790b5bee3e36efbc56d65dd1e7091a07e2d
ollioddi <oliver@roedscholer.dk>
2023-12-19 00:44:51 +0100

    Add DashboardResponse class and update MetricRepository and DashboardRepository
    Add DashboardResponse class, update MetricRepository and DashboardRepository, and create ApiKeyRepository

--- backend/src/main/java/com/aau/p3/performancedashboard/repository/MetricRepository.java
+++ backend/src/main/java/com/aau/p3/performancedashboard/repository/MetricRepository.java
@@ -10,12 +10,46 @@ import com.aau.p3.performancedashboard.model.Metric;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/**
+ * Represents a repository for managing {@link Metric} instances.
+ * Extends {@link ReactiveMongoRepository} for CRUD operations on {@link Metric} instances.
+ * Annotated with {@link Repository} to indicate its role as a repository.
+ */
 @Repository
 public interface MetricRepository extends ReactiveMongoRepository<Metric, String> {
-    
+
+    /**
+     * Retrieves all {@link Metric} instances with pagination.
+     * Uses Spring Data MongoDB's derived query feature for automatic query generation.
+     *
+     * @param pageable The pagination information.
+     * @return A {@link Flux} of {@link Metric} instances for the given page, or {@link Flux#empty()} if none exist.
+     */
+    public Flux<Metric> findAllBy(Pageable pageable);
+
+    /**
+     * Checks if a metric exists by its name.
+     *
+     * @param metricName the name of the metric.
+     * @return true if a metric with the given name exists, false otherwise.
+     */
+    public Mono<Boolean> existsByName(String metricName);
+
+    /**
+     * Checks if a metric exists by its id.
+     *
+     * @param metricId the id of the metric.
+     * @return true if a metric with the given id exists, false otherwise.
+     */
+    public Mono<Boolean> existsById(String metricId);
+        
+    /**
+     * Finds a {@link Metric} by its name.
+     * Uses derived query feature for query generation.
+     *
+     * @param metricName The name of the {@link Metric} to find.
+     * @return A {@link Mono} with the {@link Metric}, or {@link Mono#empty()} if not found.
+     */
     public Mono<Metric> findByName(String metricName);
     
-    public Mono<Boolean> existsByName(String metricName);
-    
-    public Flux<Metric> findAllBy(Pageable pageable);
 }
