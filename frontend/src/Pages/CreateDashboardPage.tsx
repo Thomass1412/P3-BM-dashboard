fc69557b074ee18610f01e96d55e790207c47ace
Thomass1412 <thomas.lunoe@gmail.com>
2023-12-20 16:03:51 +0100

    finishing touches

--- frontend/src/Pages/CreateDashboardPage.tsx
+++ frontend/src/Pages/CreateDashboardPage.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <main className="bg-green-50 h-screen flex flex-col">
         <Header />
         <div className="flex flex-1 items-center justify-center p-4">
-            <div className="w-full max-w-md"> {/* Adjust max-w-md to control the form's width */}
+            <div className="w-full max-w-md"> 
                 <DashboardForm />
             </div>
         </div>
